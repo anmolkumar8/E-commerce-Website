e8d15e32cb34d3f7fd3e0631ee1870578da9af94
Anmol Kumar <anmol.krhjp@gmail.com>
2025-07-06 17:17:54 +0530

    Delete ECommerceBackend directory

--- ECommerceBackend/ecommerce-backend/ecommerce-backend/src/main/java/com/anufa/ecommerce_backend/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.anufa.ecommerce_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.anufa.ecommerce_backend.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {}
--- ECommerceBackend/ecommerce-backend/ecommerce-backend/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO product (id, name, description, price, created_at) VALUES
-(1, 'Sample Product', 'This is a sample product.', 9.99, CURRENT_TIMESTAMP);
